840260d49cddfe3790fd5f5078fc82cb12289d86
İbrahim Alper Yiğit <ibrahimalperyigit@Ibrahims-MacBook-Air.local>
2023-03-04 17:02:49 +0300

    medium project3

--- src/test/java/tests/AmazonTest.java
+++ src/test/java/tests/AmazonTest.java
@@ -19,31 +19,32 @@ public class AmazonTest extends TestBaseReport {
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
 
         Assert.assertTrue(Driver.getDriver().getTitle().contains("Amazon"));
-        extentTest.pass("Correctly navigated to the Amazon page");
+        extentTest.info("Correctly navigated to the Amazon page");
 
 
         AmazonPage amazonPages = new AmazonPage();
 
         amazonPages.searchBox.sendKeys(ConfigReader.getProperty("searchText") + Keys.ENTER);
-        extentTest.pass("Java word was typed and searched");
+        extentTest.info("Java word was typed and searched");
 
         Assert.assertTrue(amazonPages.resultSearchingText.getText().contains("Java"));
-        extentTest.pass("Search results contain 'Java'");
+        extentTest.info("Search results contain 'Java'");
+
 
         amazonPages.firstSearchingResult.click();
-        extentTest.pass("The first sample was clicked");
+        extentTest.info("The first sample was clicked");
 
         Assert.assertTrue(amazonPages.shareButton.isDisplayed());
-        extentTest.pass("Share button is appeared");
+        extentTest.info("Share button is appeared");
 
         amazonPages.addToCartButton.click();
-        extentTest.pass("The sample was added to cart");
+        extentTest.info("The sample was added to cart");
 
         Assert.assertEquals(amazonPages.addedToCartText.getText(), "Added to Cart");
-        extentTest.pass("'Added to Cart' text was appeared");
+        extentTest.info("'Added to Cart' text was appeared");
 
         Driver.closeDriver();
-        extentTest.info("Test Completed");
+        extentTest.pass("Test Completed");
 
     }
 
